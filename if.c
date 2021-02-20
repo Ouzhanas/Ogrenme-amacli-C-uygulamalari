f13ea3353abc3fcb9b579d292b7254aa57ac6cb6
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2021-02-20 21:43:03 +0300

    Update if.c

--- if.c
+++ if.c
@@ -2,22 +2,22 @@
 
 int main(){
 	
-	int i=0;
-	int n=0;
+	int i=0;	/*int turunde 'n' ve 'i' degiskenleri tanimladik ve bunlari 0'a esitledik.*/
+	int n=0;		/*ilk degerleri 0'a esitlemek programimizin verimli ve saglikli olmasini saglar.*/
 	
 	printf("Bir sayi gir: \n");
 	scanf("%d",&n);
-	printf("\n1 2 3 rakamlarindan birini gir: \n");
+	printf("\n1 2 3 rakamlarindan birini gir: \n");	     /*bu kisimda kullanicidan int turunden degerleri istedik.*/
 	scanf("%d",&i);
 	
 	
-	if(i==1)
+	if(i==1)	/*'i' degiskeni 1'e esit oldugunda yapilacak islemi if kosulu ile belirledik*/
 	printf("sonuc = %d\n",n);
-	if(i==2)
+	if(i==2)	/*'i' degiskeni 2'e esit oldugunda yapilacak islemi if kosulu ile belirledik*/
 	printf("sonuc = %d\n",n+n);
-	if(i==3)
+	if(i==3)	/*'i' degiskeni 3'e esit oldugunda yapilacak islemi if kosulu ile belirledik*/
 	printf("sonuc = %d\n",n*n);
 		
-	system("pause");
-	return 0;
+	system("pause");   /*programin kosmasi bittikten sonra ekranda durmasini sagladik.*/
+	return 0;	/*programin kosup sonlanmasini sagladik.*/
 }
