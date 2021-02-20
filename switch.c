d4187b33c8fd726b6fc5ecd80cdb10dd619e8577
Oğuzhan <72504628+Ouzhanas@users.noreply.github.com>
2021-02-20 21:09:34 +0300

    Update switch.c

--- switch.c
+++ switch.c
@@ -2,16 +2,17 @@
 
 int main(){
 	
-	int sec;
+	int choose;	/*int turunden degisken tanimladik*/
 	
-	printf("lutfen 1 ile 3 arasinda bir secim yapiniz");
-	scanf("%d",&sec);
-	switch(sec)
+	printf("lutfen 1 ile 3 arasinda bir secim yapiniz");	/*yapilacak islemi kullaniciya soyledik*/
+	scanf("%d",&choose);
+	switch(choose)		/* switch secim yapmamiza yardimci olur*/
 	{
-		case 1:printf("birinci durum secildi");break;
-		case 2:printf("ikinci durum secildi");break;
+		case 1:printf("birinci durum secildi");break;	/*case ve sonrasinda gelen rakamlar bize bir durumu belirtirler ve o durumun secilmesi durumunda yapilacak islemi soyler.*/
+		case 2:printf("ikinci durum secildi");break;			/*break ise bir durum secildigi zaman basariyla sonlanmasini saglar*/
 		case 3:printf("ucuncu durum secildi");break;
-		default:printf("yanlis bir sayi girdiniz");
+		default:printf("yanlis bir sayi girdiniz");	/*default ise kullanici istenilen degerler girilmedigi zaman programin yapacilacak durumu soyler*/
 			}
-			return 0;
+		
+	return 0;
 }
